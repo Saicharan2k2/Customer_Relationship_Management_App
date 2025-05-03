8edb7fae929b86ed83a6360cd77503501a945c37
Kodam Sai Charan <96488659+Saicharan2k2@users.noreply.github.com>
2025-05-03 12:02:05 +0530

    Delete CRM_SPRING_BOOT_BACKEND directory

--- CRM_SPRING_BOOT_BACKEND/src/main/java/in/demo/sai/dao/IEmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package in.demo.sai.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import in.demo.sai.model.Employee;
-
-public interface IEmployeeRepository extends CrudRepository<Employee, Integer> {
-
-}
